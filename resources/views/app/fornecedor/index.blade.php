9ef83db09b9a43086821ec1e808bf026ad6bc6da
Luiz Silva <luizsilva.ee@gmail.com>
2021-04-18 17:10:30 -0300

    Criados controllers, rotas e views de fornecedores

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,16 @@
+<h3>Fornecedor</h3>
+@isset($fornecedores)
+    @forelse ($fornecedores as $indice => $fornecedor)
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? 'não informado' }}
+        <br>
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <hr>
+    @empty
+        Não existem fornecedores cadastrados
+    @endforelse
+
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ Route::prefix('/app')->group(function () {
         return 'clientes';
     })->name('app.clientes');
 
-    Route::get('/fornecedores', function () {
-        return 'fornecedores';
-    })->name('app.fornecedores');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
 
     Route::get('/produtos', function () {
         return 'produtos';
